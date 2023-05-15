e25efbefa1848894233995280adee1c013d0b9df
Thaynison <cthaynison@outlook.com.br>
2023-05-15 13:52:21 -0300

    att

--- paginas/scripts/salva.js
+++ paginas/scripts/salva.js
@@ -3,48 +3,37 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function salvarDados() {
-    // Token de acesso do GitHub
-    const token = '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';
-  
-    // URL do repositório no GitHub
     const repoUrl = 'https://api.github.com/repos/Thaynison/Thaynison';
-  
-    // Array de arquivos a serem salvos
     const arquivos = [
       { nome: 'comandos.json', dados: { /* dados comandos */ } },
       { nome: 'dados.json', dados: { /* dados dados */ } }
     ];
   
-    // Ocultar o elemento com id "pastDocument"
-    var pastDocument = document.getElementById("pastDocument");
-    pastDocument.style.display = "none";
-  
-    // Fazer o upload dos arquivos para o repositório no GitHub
     arquivos.forEach(async (arquivo) => {
       const url = `${repoUrl}/contents/documentos/${arquivo.nome}`;
+      const dadosJson = JSON.stringify(arquivo.dados);
   
       try {
-        // Converter os dados do arquivo para uma string JSON
-        const dadosJson = JSON.stringify(arquivo.dados);
-  
-        // Criar o objeto de dados para enviar para o GitHub
-        const data = {
-          message: `Upload do arquivo ${arquivo.nome}`,
-          content: Buffer.from(dadosJson).toString('base64')
-        };
-  
-        // Fazer a chamada para a API do GitHub para criar o arquivo
-        await axios.put(url, data, {
+        const response = await fetch(url, {
+          method: 'PUT',
           headers: {
-            Authorization: `Bearer ${token}`
-          }
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 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'
+          },
+          body: JSON.stringify({
+            message: `Upload do arquivo ${arquivo.nome}`,
+            content: btoa(dadosJson)
+          })
         });
   
-        console.log(`O arquivo ${arquivo.nome} foi salvo com sucesso.`);
+        if (response.ok) {
+          console.log(`O arquivo ${arquivo.nome} foi salvo com sucesso.`);
+        } else {
+          console.error(`Erro ao salvar o arquivo ${arquivo.nome}.`);
+        }
       } catch (error) {
         console.error(`Erro ao salvar o arquivo ${arquivo.nome}:`, error);
       }
     });
 }
-
   
