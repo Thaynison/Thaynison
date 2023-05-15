a75f524e4a6269c6a5112f7687911b469050d1ee
Thaynison <cthaynison@outlook.com.br>
2023-05-15 14:44:14 -0300

    Update salva.js

--- paginas/scripts/salva.js
+++ paginas/scripts/salva.js
@@ -2,70 +2,52 @@ document.addEventListener("DOMContentLoaded", function() {
     salvarDados();
 });
 
-// Função para salvar o documento no repositório do GitHub
-async function salvarDados() {
-    const token = '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';
-    const repoOwner = 'Thaynison';
-    const repoName = 'Thaynison';
-    const pasta = 'documentos';
-  
-    const comandosJson = { /* Seus dados do comandos.json */ };
-    const dadosJson = { /* Seus dados do dados.json */ };
-  
-    // Função para criar/atualizar um arquivo no repositório
-    async function criarOuAtualizarArquivo(nomeArquivo, conteudoArquivo) {
-      const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/main/${pasta}/${nomeArquivo}`;
-  
-      try {
-        // Obter o conteúdo atual do arquivo, se existir
-        const response = await fetch(apiUrl);
-        if (response.ok) {
-          const data = await response.json();
-          const fileSha = data.sha;
-          const commitMessage = `Atualizar ${nomeArquivo}`;
-  
-          await fetch(apiUrl, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-              message: commitMessage,
-              content: btoa(unescape(encodeURIComponent(conteudoArquivo))),
-              sha: fileSha
-            })
-          });
-          console.log(`${commitMessage}: ${nomeArquivo}`);
-        } else if (response.status === 404) {
-          // Arquivo não existe, criar novo arquivo
-          const commitMessage = `Criar ${nomeArquivo}`;
-  
-          await fetch(apiUrl, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-              message: commitMessage,
-              content: btoa(unescape(encodeURIComponent(conteudoArquivo)))
-            })
-          });
-          console.log(`${commitMessage}: ${nomeArquivo}`);
-        } else {
-          throw new Error('Falha ao acessar o GitHub');
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  
-    var pastDocument = document.getElementById("pastDocument");
-    pastDocument.style.display = "none";
+const token = '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';
+const repository = 'Thaynison/Thaynison';
+const folder = 'documentos/';
 
-    // Chamar a função para criar/atualizar os arquivos
-    criarOuAtualizarArquivo('comandos.json', JSON.stringify(comandosJson));
-    criarOuAtualizarArquivo('dados.json', JSON.stringify(dadosJson));
-  }
+function uploadFile(file, filename) {
+  const apiUrl = `https://api.github.com/repos/${repository}/contents/${folder}${filename}`;
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    Accept: 'application/vnd.github.v3+json'
+  };
+
+  const reader = new FileReader();
+
+  reader.onload = function(event) {
+    const content = event.target.result;
+    const encodedContent = btoa(content);
+
+    const body = {
+      message: 'Upload file',
+      content: encodedContent
+    };
+
+    fetch(apiUrl, {
+      method: 'PUT',
+      headers: headers,
+      body: JSON.stringify(body)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Arquivo enviado com sucesso:', data);
+      })
+      .catch(error => {
+        console.error('Erro ao enviar o arquivo:', error);
+      });
+  };
+
+  reader.readAsText(file);
+}
+
+function salvarDados() {
+  const fileInput = document.getElementById('documentoInput');
+  const file = fileInput.files[0];
+  const filename = file.name;
+
+  uploadFile(file, filename);
+}
+
   
